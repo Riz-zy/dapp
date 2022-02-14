3e5cf5ad8c5382a64198e00722c8cfb47fdeecd4
Rishikesh_Tekavade <rishiteks@gmail.com>
2022-02-14 22:27:19 +0530

    UI for first page amlost done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Menu from './components/Menu';
+import MiddleSection from './components/MiddleSection';
+import EndSection from './components/EndSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <MiddleSection/>
+      <EndSection/>
+    </div>
+  );
+}
+
+export default App;
